5ef35dfb6dc8533c50ad4ef3dcdc54e8b17a152e
David Prause <praused@hotmail.com>
2017-05-11 08:57:27 -0400

    DisplayOrder() wrapper for CompareTo()

--- StringIntCompare/Program.cs
+++ StringIntCompare/Program.cs
@@ -14,9 +14,20 @@ namespace StringIntCompare
             string apple = "apple";
             string pear = "pear";
 
-            Console.WriteLine(apple.CompareTo(pear));
-            Console.WriteLine(pear.CompareTo(apple));
-            Console.WriteLine(apple.CompareTo(apple));
+            DisplayOrder(apple, pear);
+            DisplayOrder(pear, apple);
+            DisplayOrder(apple, apple);
+        }
+
+        static void DisplayOrder<T>(T x, T y) where T : IComparable<T>
+        {
+            int result = x.CompareTo(y);
+            if (result == 0)
+                Console.WriteLine("{0,12} = {1}", x, y);
+            else if (result > 0)
+                Console.WriteLine("{0,12} > {1}", x, y);
+            else
+                Console.WriteLine("{0,12} < {1}", x, y);
         }
     }
 }
